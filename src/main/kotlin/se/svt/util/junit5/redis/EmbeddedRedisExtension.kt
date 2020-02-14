24c9dc291a871fff7fedf58669cc47e24d7fc379
Gustav Grusell <gustav.grusell@svt.se>
2020-02-14 11:11:16 +0100

    Code review

--- src/main/kotlin/se/svt/util/junit5/redis/EmbeddedRedisExtension.kt
+++ src/main/kotlin/se/svt/util/junit5/redis/EmbeddedRedisExtension.kt
@@ -1,38 +1,32 @@
 package se.svt.util.junit5.redis
 
 import me.alexpanov.net.FreePortFinder
-import org.junit.jupiter.api.extension.AfterAllCallback
 import org.junit.jupiter.api.extension.BeforeAllCallback
 import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.api.extension.ExtensionContext.Namespace
 import redis.embedded.RedisServer
 
 const val REDIS_URI_PROPERTY = "redis.uri"
+const val REDIS_PORT_PROPERTY = "embedded-redis.port"
 
-class EmbeddedRedisExtension(private val forceRandomPort: Boolean = false) : BeforeAllCallback, AfterAllCallback {
+class EmbeddedRedisExtension(private val reusePort: Boolean = false) : BeforeAllCallback {
 
     lateinit var redisServer: RedisServer
 
-    override fun beforeAll(context: ExtensionContext?) {
+    override fun beforeAll(context: ExtensionContext) {
         val port =
-                if (forceRandomPort) FreePortFinder.findFreeLocalPort()
+                if (!reusePort) FreePortFinder.findFreeLocalPort()
                 else findPortFromSystemProperty() ?: FreePortFinder.findFreeLocalPort()
-        System.setProperty(REDIS_URI_PROPERTY, "redis://localhost:$port")
+        System.setProperty(REDIS_PORT_PROPERTY, port.toString())
         redisServer = RedisServer(port)
         redisServer.start()
 
-        if (context != null) {
-            val wrapper = RedisWrapper(redisServer)
-            context.getStore(Namespace.create(EmbeddedRedisExtension::class.java))
-                    .put("redis", wrapper)
-            Runtime.getRuntime().addShutdownHook(Thread(Runnable {
-                wrapper.close()
-            }))
-        }
-    }
-
-    override fun afterAll(context: ExtensionContext?) {
-        redisServer.stop()
+        val wrapper = RedisWrapper(redisServer)
+        context.getStore(Namespace.create(EmbeddedRedisExtension::class.java))
+                .put("redis", wrapper)
+        Runtime.getRuntime().addShutdownHook(Thread(Runnable {
+            wrapper.close()
+        }))
     }
 
     class RedisWrapper(private val redis: RedisServer) : ExtensionContext.Store.CloseableResource {
