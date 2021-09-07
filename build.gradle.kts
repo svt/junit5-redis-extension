d6946c795a868a1960b329c868490d8e6f2db7c1
Josef Andersson <josef.andersson@svt.se>
2021-09-07 17:27:12 +0200

    fix: bump relevant dependencies
    Signed-off-by: Josef Andersson <josef.andersson@svt.se>

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,13 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("maven-publish")
     idea
-    kotlin("jvm") version "1.4.32"
-    kotlin("kapt") version "1.4.32"
-    kotlin("plugin.spring") version "1.4.32"
-    id("org.jmailen.kotlinter") version "3.4.3"
-    id("pl.allegro.tech.build.axion-release") version "1.10.2"
-    id("se.ascp.gradle.gradle-versions-filter") version "0.1.8"
-    id("se.svt.oss.gradle-yapp-publisher-plugin") version "0.1.13"
+    kotlin("jvm") version "1.5.21"
+    kotlin("kapt") version "1.5.21"
+    kotlin("plugin.spring") version "1.5.21"
+    id("org.jmailen.kotlinter") version "3.6.0"
+    id("pl.allegro.tech.build.axion-release") version "1.13.3"
+    id("se.ascp.gradle.gradle-versions-filter") version "0.1.10"
+    id("se.svt.oss.gradle-yapp-publisher-plugin") version "0.1.15"
     id("signing")
 }
 
@@ -25,19 +25,14 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("reflect", "1.4.32"))
-
-    api("com.squareup.okhttp3:mockwebserver:3.10.0")
+    implementation(kotlin("reflect", "1.5.21"))
     implementation("me.alexpanov:free-port-finder:1.1.1")
     implementation("it.ozimov:embedded-redis:0.7.3")
-
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-    testImplementation("org.assertj:assertj-core:3.18.1")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
-    implementation("org.junit.jupiter:junit-jupiter-engine:5.5.2")
-    implementation("org.junit.jupiter:junit-jupiter-api:5.5.2")
-    implementation("org.junit.jupiter:junit-jupiter-params:5.5.2")
-    testImplementation("io.mockk:mockk:1.9.3")
+    implementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
+    implementation("org.junit.jupiter:junit-jupiter-engine:5.7.2")
+    testImplementation("redis.clients:jedis:3.7.0")
+    testImplementation("org.assertj:assertj-core:3.20.2")
+    testImplementation("io.mockk:mockk:1.12.0")
 }
 
 tasks.withType<KotlinCompile> {
