420ea211735b23ecb9f583728f7bc041c105a67e
Josef Andersson <josef.andersson@svt.se>
2022-05-22 22:17:31 +0200

    build: update publishing and properties
    Signed-off-by: Josef Andersson <josef.andersson@svt.se>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    `maven-publish`
     `java-library`
-    `signing`
     `idea`
     kotlin("jvm") version "1.6.21"
     kotlin("kapt") version "1.6.21"
@@ -11,7 +9,7 @@ plugins {
     id("org.jmailen.kotlinter") version "3.10.0"
     id("pl.allegro.tech.build.axion-release") version "1.13.7"
     id("se.ascp.gradle.gradle-versions-filter") version "0.1.16"
-    id("se.svt.oss.gradle-yapp-publisher-plugin") version "0.1.15"
+    id("se.svt.oss.gradle-yapp-publisher") version "0.1.18"
 }
 
 scmVersion.tag.prefix = "release"
